01dab946aa9186faf1785db1c04bfc09b91e1f07
Sarmat <Spirator@Yandex.ru>
2023-09-17 13:55:00 +0700

    initial commit

--- Task_2.py
+++ Task_2.py
@@ -2,3 +2,19 @@
                               Задача 42: 
 Узнать какая максимальная households в зоне минимального значения population.
 """
+
+import pandas as pd
+
+
+data = pd.read_csv('california_housing_train.csv')
+
+min_population = data['population'].min()
+
+print(min_population)
+
+filtered_data = data[data['population'] == min_population]
+
+max_households = filtered_data['households'].max()
+
+print(f"Максимальное количество домохозяйств в зоне с минимальным значением населения: {max_households}")
+
